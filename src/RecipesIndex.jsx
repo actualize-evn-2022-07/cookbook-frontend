048daaa3d5a6d4a225c451fe66f9c1bc574e055b
Peter Jang <peter.x.jang@gmail.com>
2022-10-12 20:58:26 -0500

    Style more stuff (navbar, background image, font, grid of cards)

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -2,15 +2,20 @@ export function RecipesIndex(props) {
   return (
     <div id="recipes-index">
       <h1>All recipes</h1>
-      <div className="recipes row">
+
+      <div className="row">
         {props.recipes.map((recipe) => (
-          <div className="col-4" key={recipe.id}>
-            <h2>{recipe.title}</h2>
-            <img src={recipe.image_url} alt="" />
-            <p>Chef: {recipe.chef}</p>
-            <button className="btn btn-info" onClick={() => props.onSelectRecipe(recipe)}>
-              More info
-            </button>
+          <div className="col-md-3 mb-4" key={recipe.id}>
+            <div className="card">
+              <img src={recipe.image_url} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{recipe.title}</h5>
+                <p className="card-text">Chef: {recipe.chef}</p>
+                <a href="#" className="btn btn-primary" onClick={() => props.onSelectRecipe(recipe)}>
+                  More info
+                </a>
+              </div>
+            </div>
           </div>
         ))}
       </div>
