7c664ba0c2c7248d4f60ce19d1c52ebb9b4e82ca
Peter Jang <peter.x.jang@gmail.com>
2022-10-13 21:12:04 -0500

    Change form to use JavaScript for web request

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,8 +1,25 @@
+import axios from "axios";
+
 export function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit");
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/users.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
+
   return (
     <div id="signup">
       <h1>Signup</h1>
-      <form method="POST" action="http://localhost:3000/users.json">
+      <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" className="form-control" type="text" />
         </div>
