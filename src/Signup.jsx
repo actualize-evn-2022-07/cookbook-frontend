3866e16e328c389dd997b2bf4e7a16650cf7b54b
Peter Jang <peter.x.jang@gmail.com>
2022-10-13 21:32:42 -0500

    Display signup errors on the page instead of just in the javascript console

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,24 +1,34 @@
 import axios from "axios";
+import { useState } from "react";
 
 export function Signup() {
+  const [errors, setErrors] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("handleSubmit");
+    setErrors([]);
     const params = new FormData(event.target);
     axios
-      .post("http://localhost:3000/users.json", params)
+      .post("http://localhost:3000/user.json", params)
       .then((response) => {
         console.log(response.data);
         event.target.reset();
+        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
       })
       .catch((error) => {
         console.log(error.response.data.errors);
+        setErrors(error.response.data.errors);
       });
   };
 
   return (
     <div id="signup">
       <h1>Signup</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
       <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" className="form-control" type="text" />
