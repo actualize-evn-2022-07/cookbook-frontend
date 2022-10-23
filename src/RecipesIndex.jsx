0f252e0c05245089b911830dcd9935647df54e53
Peter Jang <peter.x.jang@gmail.com>
2022-10-23 10:31:30 -0500

    Add dynamic search filter for recipes index

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,23 +1,30 @@
+import { useState } from "react";
+
 export function RecipesIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div id="recipes-index">
       <h1>All recipes</h1>
-
+      Search filter:{" "}
+      <input value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} type="text" />
       <div className="row">
-        {props.recipes.map((recipe) => (
-          <div className="col-md-3 mb-4" key={recipe.id}>
-            <div className="card">
-              <img src={recipe.image_url} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">{recipe.title}</h5>
-                <p className="card-text">Chef: {recipe.chef}</p>
-                <a className="btn btn-primary" onClick={() => props.onSelectRecipe(recipe)}>
-                  More info
-                </a>
+        {props.recipes
+          .filter((recipe) => recipe.title.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map((recipe) => (
+            <div className="col-md-3 mb-4" key={recipe.id}>
+              <div className="card">
+                <img src={recipe.image_url} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{recipe.title}</h5>
+                  <p className="card-text">Chef: {recipe.chef}</p>
+                  <a className="btn btn-primary" onClick={() => props.onSelectRecipe(recipe)}>
+                    More info
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
